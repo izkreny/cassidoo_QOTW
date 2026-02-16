6caf88dd6761dec5c3dca0f17d55f6c0ab4e39fc
izkreny <iskren.nemet@gmail.com>
2026-02-16 11:34:40 +0100

    fix: add version without copy

--- answers/code/20260209_443.rb
+++ answers/code/20260209_443.rb
@@ -8,13 +8,22 @@ require_relative "../../test_helper"
 # > Bonus: do this without making a copy of the array!
 #
 class Example < Minitest::Test
-  def test_example = assert_equal [2, 3, 10, 0, 0], move_numbers([0, 2, 0, 3, 10], 0)
+  def test_example_orig = assert_equal [2, 3, 10, 0, 0], move_numbers([0, 2, 0, 3, 10], 0)
+  def test_example_sort = assert_equal [2, 3, 10, 0, 0], move_numbers_([0, 2, 0, 3, 10], 0)
 end
 
-# ## Answer
+# ## Answers
 #
 # Methods used:
 # - [Enumerable#partition](https://docs.ruby-lang.org/en/master/Enumerable.html#method-i-partition)
 # - [Array#flatten](https://docs.ruby-lang.org/en/master/Array.html#method-i-flatten)
+# - [Array#sort!](https://docs.ruby-lang.org/en/master/Array.html#method-i-sort-21)
 #
+# ### Original
 def move_numbers(integer_array, number) = integer_array.partition { it != number }.flatten
+
+# ### Sort only!
+#
+# Slightly modified version from the [Ruby Users Forum](https://www.rubyforum.org/t/cassidoo-s-interview-question-of-the-week-443/98/3)
+# Should this solution qualify as the only one where copy of the array is not made? :)
+def move_numbers_(integer_array, number) = integer_array.sort! { it == number ? 1 : 0 }
